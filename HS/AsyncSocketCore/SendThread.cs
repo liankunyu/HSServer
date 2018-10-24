2d0f30a48b1ec18bd146eb8971ba313dbb9d669c
liankunyu <liankunyu@outlook.com>
2018-10-24 21:48:46 +0800

    改写SendThread类，利用数据库执行下发。

--- HS/AsyncSocketCore/SendThread.cs
+++ HS/AsyncSocketCore/SendThread.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -25,11 +27,16 @@ namespace HS
         {
             try
             {
-                //存储发送数据的表
-                List<byte> listSend = new List<byte>();
-                //进行循环遍历表中的指令下发
+                ////存储发送数据的表
+                //List<byte> listSend = new List<byte>();
+                ////进行循环遍历表中的指令下发
+                DataTable dt = new DataTable();
+                string hs_sql, cmd, path, newPath;
+                int device_ID;
+                byte[] buf = { 0X68, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
                 while (true)
                 {
+                    #region
                     //if (m_asyncSocketServer.Redis.ListLength("commands") > 0)
                     //{
                     //    #region
@@ -84,7 +91,7 @@ namespace HS
                     //            listSend.Clear();
                     //            Thread.Sleep(50);
                     //        }
-                            
+
                     //    }
                     //    catch
                     //    {
@@ -95,6 +102,50 @@ namespace HS
                     //{
                     //    Thread.Sleep(50);
                     //}
+                    #endregion
+
+                    hs_sql = "select * from SendTable";
+                    dt = DbHelperSQL.OpenTable(hs_sql);
+                    if (dt.Rows.Count > 0)
+                    {
+                        #region
+                        hs_sql = "Truncate table SendTable";//清除表中所有行，保留表结构、与delete类似比delete速度快,而且效率高，使用的系统和事务日志资源少
+                        DbHelperSQL.ExecuteSql(hs_sql);
+                        for (int i=0;i<dt.Rows.Count;i++)
+                        {
+                            device_ID = Convert.ToInt32(dt.Rows[i][2]);
+                            cmd = "select pathName from DeviceInfo where DeviceID='" + device_ID + "'";//这一步可以不用，直接用变量加路径
+                            path = DbHelperSQL.Execute(cmd).Trim();
+                            newPath = Path.Combine(path, "tmp.xml");
+                            using (FileStream fsRead = new FileStream(newPath, FileMode.Open, FileAccess.Read))
+                            {
+                                byte[] bf = new byte[1024 * 100];
+                                int r = fsRead.Read(bf, 0, bf.Length);
+                                byte[] copy = new byte[r];
+                                List<byte> listSend = new List<byte>();
+                                listSend.AddRange(buf);
+                                listSend.Add((byte)(r & 0x00FF));//低字节
+                                listSend.Add((byte)(r >> 8));//高字节
+                                Array.Copy(bf, copy, r);
+                                listSend.AddRange(copy);
+                                byte[] CRC = AuxiliaryMethod.crc16(listSend.ToArray(), listSend.Count);//调用校验
+                                listSend.AddRange(CRC);
+                                listSend.Add(0x16);
+                                AsyncSocketUserToken UserToken = m_asyncSocketServer.AsyncSocketUserTokenList.UseKey(device_ID);
+                                lock (UserToken)
+                                {
+                                    UserToken.ConnectSocket.Send(listSend.ToArray());
+                                }
+                                listSend.Clear();
+                            }
+                        }
+
+                        #endregion
+                    }
+                    else
+                    {
+                        Thread.Sleep(50);
+                    }
                 }
             }
             catch (Exception e)
